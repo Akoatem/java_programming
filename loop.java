528e240189436c43591dfe5d3f4e8f794499815c
Ako Atem <71437843+Akoatem@users.noreply.github.com>
2022-12-31 09:19:51 +0200

    write a program to print number using while loop from 0 to 10

---
+++ loop.java
@@ -0,0 +1,15 @@
+
+/*
+ write a program to print number using while loop from 0 to 10
+ */
+public class loop {
+    public static void main(String args[])
+    {
+        int count = 0;
+
+        while (count <= 10){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
